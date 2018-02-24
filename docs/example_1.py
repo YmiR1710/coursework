921c7d4f24cd028466830111e42da77a172aebf0
YmiR1710 <34999251+YmiR1710@users.noreply.github.com>
2018-02-24 23:49:43 +0200

    Delete example_1.py

--- docs/example_1.py
+++
@@ -1,8 +0,0 @@
-#small example of using tmdb API
-import tmdbsimple
-tmdbsimple.API_KEY = '0d77f0dc778bfd9a40a934c56e81b487'
-search = tmdbsimple.Search()
-a = input()
-result = search.movie(query=a)
-for s in search.results:
-    print(s['title'], s['id'], s['release_date'], s['popularity'])
