9f5864e7cab9f52301f30f5a79984a74b3c74190
YmiR1710 <34999251+YmiR1710@users.noreply.github.com>
2018-05-28 15:27:39 +0300

    Delete main.py

--- main.py
+++
@@ -1,8 +0,0 @@
-from progress_tester import Visualization
-from films import search
-
-try:
-    Visualization.visualize_1()
-    Visualization.updater()
-except:
-    Visualization.visualize_2("ERROR")
